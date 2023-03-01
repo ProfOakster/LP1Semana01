199ec28d124079d21ab4dbb24040666443689b0d
TomasCarvalho <2606tom@gmail.com>
2023-03-01 20:49:10 +0000

    Add Documentation

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,15 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// Test program
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Prints "Hello, World!" and "Bye, World!" on the console
+        /// </summary>
+        /// <param name="args"> unused </param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
