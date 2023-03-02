7a61180200f185d0678f096125aeea3d10db3dc1
TomasCarvalho <2606tom@gmail.com>
2023-03-02 21:45:18 +0000

    Add VariasStrings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string steamed="Oh no\u274C, that isn't smoke.";
+            string hams = "It's steam\u2705, from the Steamed Hams\u2122 we're having.";
+            string borealis = "In this time of year\nIn this time of day";
+
+            string scout = @"Allow C:\tierra\qliphort.exe ? <Y/N>...[Y]";
+
+            Console.WriteLine(steamed);
+            Console.WriteLine(hams);
+            Console.WriteLine(borealis);
+            Console.WriteLine(scout);
+        }
+    }
+}
