a09f3cf1ae83736b64ff13e58317437b1bccfeca
TomasCarvalho <2606tom@gmail.com>
2023-03-02 21:01:47 +0000

    Add TiposVarios; Integer variables created

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            sbyte neg_smallest = -1;
+            short neg_small = -1337;
+            int neg_average = -1984;
+            long neg_big = -5318008L;
+            byte smallest = 1;
+            ushort small = 1337;
+            uint average = 1984U;
+            ulong big = 5318008UL;
+
+            Console.WriteLine(neg_smallest);
+            Console.WriteLine(neg_small);
+            Console.WriteLine(neg_average);
+            Console.WriteLine(neg_big);
+            Console.WriteLine(smallest);
+            Console.WriteLine(small);
+            Console.WriteLine(average);
+            Console.WriteLine(big);
+
+
+
+
+        }
+    }
+}
